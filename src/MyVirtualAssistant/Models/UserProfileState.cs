ec6f33456bbcca3149706166a1db78ef50ecfd26
Chris Wiederspan <chris@wiederspan.com>
2020-03-13 23:38:53 +0000

    Initial code project added

---
+++ src/MyVirtualAssistant/Models/UserProfileState.cs
@@ -0,0 +1,8 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace MyVirtualAssistant.Models {
+    public class UserProfileState {
+        public string Name { get; set; }
+    }
+}
---
+++ src/MyVirtualAssistant/Services/BotSettings.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Bot.Solutions;
+
+namespace MyVirtualAssistant.Services {
+    public class BotSettings : BotSettingsBase {
+    }
+}
